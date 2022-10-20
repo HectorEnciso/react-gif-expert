f6c07bae345926a38652902c05d67a60a73eb8b2
Giovanni Daniel Fabela Rodriguez <daniel_gdfr@hotmail.com>
2022-10-20 13:31:51 -0500

    Primer Commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+export const AddCategory = ({ onNewCategory }) => {
+
+    const [ inputValue, setInputValue ] = useState('');
+
+    const onInputChange = ({target}) => {
+        setInputValue(target.value);
+    }
+
+    const onSubmit = ( event ) => {
+      event.preventDefault();
+      // console.log(inputValue);
+      
+      if (inputValue.trim().length <= 1) return;
+
+      //setCategories(cateories => [ inputValue, ...cateories ]);
+      onNewCategory( inputValue.trim() );
+      setInputValue('');
+    }
+
+  return (
+    <form onSubmit={ (event) => onSubmit(event) }>
+        <input
+            type="text"
+            placeholder="Buscar gifs"
+            value={ inputValue }
+            onChange= { onInputChange }
+        />
+    </form>
+  )
+}
